463cec7734ad7a67d62e9ce0744183e3e1013503
Marcin Lewandowskig <marcin@radiokit.org>
2017-01-07 00:46:53 +0100

    Bumped to v1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Jumbo.Mixfile do
 
   def project do
     [app: :jumbo,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      description: "Jumbo Job Queue",
