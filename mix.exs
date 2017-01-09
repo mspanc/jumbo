6c995a0098fdee479a24b435f1b8870311ebdd59
Marcin Lewandowskig <marcin@radiokit.org>
2017-01-09 01:16:21 +0100

    Bumped to 1.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Jumbo.Mixfile do
 
   def project do
     [app: :jumbo,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      name: "Jumbo",
